102cdffdd94ea8d068280bf02b5e7863488b103a
Fagner Tranquilino <fagnertds@gmail.com>
2025-07-23 15:51:43 -0300

    criadas as entities, e o contexto para acesso ao banco de dados

---
+++ ApiCatalogo/ApiCatalogo/Model/Categoria.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiCatalogo.Model;
+
+public class Categoria
+{
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
+    [Key]
+    public int CategoriaId { get; set; }
+
+    [Required(ErrorMessage = "O Nome é obrigatório")]
+    [StringLength(200), ]
+    public string? Nome { get; set; }
+
+    [StringLength(300)]
+    public string? ImagemUrl { get; set; }
+
+    public ICollection<Produto> Produtos { get; set; }
+}

32d7dfa15e07a38c09ae8b85053c6c00400b2e77
Fagner Tranquilino <fagnertds@gmail.com>
2025-07-24 19:31:37 -0300

    Crud de Categoria

---
+++ ApiCatalogo/Controllers/CategoriaController.cs
@@ -0,0 +1,83 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetAll()
+        {
+            var categorias = await _context.Categorias.AsNoTracking().ToListAsync();
+
+            if(!categorias.Any())
+                return NotFound();
+            return Ok(categorias);
+        }
+
+        [HttpGet("{id}", Name = "ObterCategoria")]
+        public async Task<ActionResult<Categoria>> GetById(int id)
+        {
+            var categoria = await _context.Categorias.FirstOrDefaultAsync(c => c.CategoriaId == id);
+
+            if (categoria is null)
+                return NotFound("Categoria não encontrada");
+
+            return Ok(categoria);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Categoria>> Post(Categoria categoria)
+        {
+            if(categoria is null)
+                return BadRequest();
+
+            _context.Categorias.Add(categoria);
+            await _context.SaveChangesAsync();
+
+            return new CreatedAtRouteResult("ObterCategoria",
+                new { id = categoria.CategoriaId }, categoria);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(Categoria categoria, int id)
+        {
+            if (id != categoria.CategoriaId)
+                return BadRequest();
+
+            _context.Entry(categoria).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduto(int id)
+        {
+            var categoria = await _context.Categorias.FindAsync(id);
+
+            if (categoria is null)
+                return NotFound("Categoria não encontrada");
+
+            _context.Remove(categoria);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+
+}

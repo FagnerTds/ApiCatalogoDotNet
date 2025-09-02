a26966556e13d0a6a5c657267d89d15c26f72dbd
Intern01 <intern01@mirasoft.com.br>
2025-09-02 16:15:03 -0300

    método create Roles

--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -17,10 +17,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(options =>
-{
-    options.Filters.Add(typeof(ApiExceptionFilter));
-})
+builder.Services.AddControllers(//options =>
+//{
+//    options.Filters.Add(typeof(ApiExceptionFilter));
+//})
+)
 .AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
